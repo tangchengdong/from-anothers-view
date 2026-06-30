1ad89472d2b56afe99d8662e1d3fcf30b803421d
tangchengdong <t*********@*********>
2026-06-30 08:38:21 +0800

    build: add support for both GitHub Pages and IGA Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/from-anothers-view/' : '/',
+  base: process.env.DEPLOY_TARGET === 'github' ? '/from-anothers-view/' : '/',
   server: {
     port: 3000,
     strictPort: true
